f6c3f784f5989f7c8524f92a3f55ca73a6a482de
Moch Shultan A.S <62864719+mochshultan@users.noreply.github.com>
2025-04-24 13:36:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,12 @@ import xml.etree.ElementTree as ET
 import numpy as np
 import mediapipe as mp
 from sklearn.tree import DecisionTreeClassifier
+from utils import calculate_angle
 
 countLeft = 0
 countRight = 0
 prevActionLeft = False
 prevActionRight = False
-
-# Fungsi untuk menghitung sudut
-def calculate_angle(a, b, c):
-    a = np.array(a)
-    b = np.array(b)
-    c = np.array(c)
-    ba = a - b
-    bc = c - b
-    cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
-    angle = np.arccos(np.clip(cosine_angle, -1.0, 1.0))
-    return np.degrees(angle)
 
 # Download Dataset from: kagglehub.dataset_download("trainingdatapro/pose-estimation")
 # Path ke dataset
